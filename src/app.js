d35d6a4b19a4f1479eeaf85c79750a27cec97767
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-05-08 23:48:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,33 +1,51 @@
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose');
+// const users   =require("../models/user.js");
+
+// //Router Middlewares
+// app.use(express.json());
+
+// //Type of query (Hint)
+
+// /*
+
+// 1. / --> this means we need to consider all users
+// 2. /?name=swa --> Will return count of all the user name that have prefix swa. We will (Swaraj Jain, Swarak agrawal, etc). 
+// 3. /?name= -->this means we need to consider all users
+
+// */
+
+
+// // Complete this Route which will return the count of Number of Prefixmatch for the name in the query/
+
+// app.get("/",async function(req,res){
+
+//     var count = 0;
+
+//     //Write you code here
+//     //update count variable
+
+//     res.send(JSON.stringify(count));
+
+// });
+
+// module.exports = app;
+
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const users   =require("../models/user.js");
+const users = require("../models/user.js");
 
-//Router Middlewares
+// Router Middlewares
 app.use(express.json());
 
-//Type of query (Hint)
-
-/*
-
-1. / --> this means we need to consider all users
-2. /?name=swa --> Will return count of all the user name that have prefix swa. We will (Swaraj Jain, Swarak agrawal, etc). 
-3. /?name= -->this means we need to consider all users
-
-*/
-
-
-// Complete this Route which will return the count of Number of Prefixmatch for the name in the query/
-
-app.get("/",async function(req,res){
-
-    var count = 0;
-
-    //Write you code here
-    //update count variable
-
-    res.send(JSON.stringify(count));
-
+// Route to get the count of users whose name starts with the given prefix
+app.get("/", async function(req, res) {
+  let namePrefix = req.query.name || "";
+  let count = await users.countDocuments({ name: { $regex: "^" + namePrefix, $options: "i" } });
+  res.send(JSON.stringify(count));
 });
 
 module.exports = app;
